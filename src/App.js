296e4e56f0084ffcad16a3b2475c9f64edfdb7f0
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-08-28 17:20:21 +0530

    dfghj

--- src/App.js
+++ src/App.js
@@ -12,6 +12,20 @@ import PrivateRoute from './components/PrivateRoute'; // Import the PrivateRoute
 import Account from './pages/account/Account';
 import Withdraw from'./pages/wallet/Withdraw';
 import WithdrawHistory from'./pages/wallet/WithdrawHistory';
+import Rule from'./pages/activity/Rule';
+import InvitationBonu from './pages/activity/InvitationBonu';
+import Laundry from './pages/activity/Laundry';
+import SuperJackpot from './pages/activity/SuperJackpot';
+import RedeemGift from './pages/activity/RedeemGift';
+import DailySignI from './pages/activity/DailySignI';
+import Rules from './pages/activity/Rules';
+import Record from './pages/activity/Record';
+import Records from './pages/activity/Records';
+import Superjackpot_rule from './pages/activity/Superjackpot_rule';
+import SuperJackpot_star from './pages/activity/SuperJackpot_star';
+import DailyTasks from './pages/activity/DailyTasks';
+
+
 
 function App() {
   return (
@@ -40,6 +54,21 @@ function App() {
         <Route path="/wingo" element={<PrivateRoute><Wingo/></PrivateRoute>}/>
         <Route path="/wallet" element={<PrivateRoute><Withdraw/></PrivateRoute>}/>
         <Route path="/wallet" element={<PrivateRoute><WithdrawHistory/></PrivateRoute>}/>
+        <Route path="/activity/Rule" element={<PrivateRoute><Rule/></PrivateRoute>}/>
+        <Route path="/main/InvitationBonu" element={<PrivateRoute><InvitationBonu/></PrivateRoute>}/>
+        <Route path="/main/Laundry" element={<PrivateRoute><Laundry/></PrivateRoute>}/>
+        <Route path="/main/SuperJackpot" element={<PrivateRoute><SuperJackpot/></PrivateRoute>}/>
+        <Route path="/main/RedeemGift" element={<PrivateRoute><RedeemGift/></PrivateRoute>}/>
+        <Route path="/activity/DailySignI" element={<PrivateRoute><DailySignI/></PrivateRoute>}/>
+        <Route path="/activity/DailySignI/Rules" element={<PrivateRoute><Rules/></PrivateRoute>}/>
+        <Route path="/activity/DailySignI/Record" element={<PrivateRoute><Record/></PrivateRoute>}/>
+        <Route path="/main/InvitationBonus/Records" element={<PrivateRoute><Records/></PrivateRoute>}/>
+        <Route path="/main/SuperJackpot/rule" element={<PrivateRoute><Superjackpot_rule/></PrivateRoute>}/>
+        <Route path="/main/SuperJackpot/star" element={<PrivateRoute><SuperJackpot_star/></PrivateRoute>}/>
+        <Route path="/activity/DailyTasks" element={<PrivateRoute><DailyTasks/></PrivateRoute>}/>
+
+
+
         <Route
           path="/account"
           element={
@@ -55,6 +84,7 @@ function App() {
               <Withdraw />
             </PrivateRoute>
           }
+          
         />
           <Route
           path="/wallet/WithdrawHistory"
