0dcdcd681894d15085cd80b506bb68f58467756e
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-09-02 15:36:56 +0530

    hyj

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ import ActivityDetail from './pages/activity/ActivityDetail';
 import FirstRecharge from './pages/activity/FirstRecharge';
 import RebateRatio from './pages/promotion/RebateRatio';
 import Server from './pages/promotion/Server';
-import AddUSDT from './pages/wallet/AddUSDT';
-import TransAction from './pages/wallet/TransAction';
+import BetRecords from './pages/account/BetRecords';
+
 
 
 
@@ -108,8 +108,10 @@ function App() {
         <Route path="/activity/FirstRecharge" element={<PrivateRoute><FirstRecharge/></PrivateRoute>}/>
         <Route path="/promotion/RebateRatio" element={<PrivateRoute><RebateRatio/></PrivateRoute>}/>
         <Route path="/promotion/Server" element={<PrivateRoute><Server/></PrivateRoute>}/>
-        <Route path="/wallet/Withdraw/AddUSDT" element={<PrivateRoute><AddUSDT/></PrivateRoute>}/>
-        <Route path="/wallet/TransAction" element={<PrivateRoute><TransAction/></PrivateRoute>}/>
+        <Route path="/home/AllLotteryGames" element={<PrivateRoute><AllLotteryGames/></PrivateRoute>}/>
+        <Route path="/main/BetRecords" element={<PrivateRoute><BetRecords/></PrivateRoute>}/>
+
+
 
 
 
