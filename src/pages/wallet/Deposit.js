f415ae9d28090e54b2ef680db839e501623df5c7
SAHILDEWAN23 <sahildewan2323@gmail.com>
2024-09-10 13:06:01 +0530

    ok

--- src/pages/wallet/Deposit.js
+++ src/pages/wallet/Deposit.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Api from '../../services/Api';
+import Loader from '../../components/Loader';
 
 export default function Deposit(){
 const [activeSection, setActiveSection] = useState('UpiTransfer');
 
 const [usdtValue, setUsdtValue] = useState('');
+const [totalMoney, setTotalMoney] = useState(0);
+const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
 
 
 const navigate = useNavigate();
@@ -17,8 +23,56 @@ const showSection = (sectionId) => {
     setUsdtValue(value); // Update selected USDT value
   };
 
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await Api.get('/api/webapi/GetUserInfo');
+        if (response.data.status) {
+          setTotalMoney(response.data.data.money_user);
+         
+        } else {
+          setError('Failed to fetch user info');
+        }
+      } catch (err) {
+        setError('Error fetching user info');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchUserInfo();
+  }, []);
 
+  const handleDeposit = async () => {
+    let typeid = 'USDT(TRC20)';
+  
+    if (usdtValue * 90 < 900) {
+      setError("Minimum Deposit amount is 900");
+      return;
+    }
+  
+    if (activeSection === 'UsdtBep' || activeSection ==='UsdtBep1' ) {
+      typeid = 'USDT(TRC20)';
+    }
+  
+    try {
+      const response = await Api.post('/api/webapi/createPayment10', {
+        money: usdtValue * 90,
+        typeid: typeid,
+      });
+  
+      if (response.data.status === true) {
+        // Navigate to the ConfirmDeposit page and pass the response.data.datas
+        navigate('/deposit/ConfirmDeposit', { state: { data: response.data.datas } });
+      } else {
+        setError("Payment creation failed. Please try again.");
+      }
+    } catch (err) {
+      console.error('An error occurred:', err);
+      setError('An error occurred. Please try again.');
+    }
+  };
  
 
  return(
@@ -179,7 +233,7 @@ const showSection = (sectionId) => {
               </div>
             </div>
             <div data-v-98c90f53="" className="balanceAssets__main">
-              <p data-v-98c90f53="">₹56.05</p>
+              <p data-v-98c90f53="">₹{totalMoney}</p>
               <img
                 data-v-98c90f53=""
                 src="/assets/png/refresh-8e0efe26.png"
@@ -484,8 +538,8 @@ const showSection = (sectionId) => {
               >
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item active"
-            
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 10 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('10')}          
                 >                
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>10
@@ -493,8 +547,8 @@ const showSection = (sectionId) => {
                 </div>
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item"
-            
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 50 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('50')}          
                 >                
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>50
@@ -502,8 +556,8 @@ const showSection = (sectionId) => {
                 </div>
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item"
-            
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 100 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('100')}          
                 >                
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>100
@@ -511,8 +565,8 @@ const showSection = (sectionId) => {
                 </div>
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item"
-            
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 200 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('200')}          
                 >                
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>200
@@ -520,8 +574,8 @@ const showSection = (sectionId) => {
                 </div>
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item"
-            
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 500 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('500')}          
                 >                
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>500
@@ -529,9 +583,9 @@ const showSection = (sectionId) => {
                 </div>
                 <div
                   data-v-9e03166f=""
-            className="Recharge__content-paymoney__money-list__item"
-            
-                >                
+                  className={`Recharge__content-paymoney__money-list__item ${usdtValue == 1000 ? 'active' : ''}`}  
+                  onClick={() => handleUsdtClick('1000')}          
+                >                 
                   <div data-v-9e03166f="" className="amount">
                     <span data-v-9e03166f=""><img data-v-98c90f53="" src="/assets/png/usdt.png" alt="" style={{width: '20px'}} /></span>1K
                   </div>                  
@@ -876,7 +930,7 @@ const showSection = (sectionId) => {
                 
               </div>
               
-              <div data-v-9e03166f="" className="Recharge__container-rechageBtn rechage_active">
+              <div data-v-9e03166f="" className="Recharge__container-rechageBtn rechage_active" onClick={handleDeposit}>
                 Deposit
               </div>
             </div>
